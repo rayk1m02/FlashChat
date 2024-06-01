8c6573e2825047194364f988afcdc05148bfb959
rayk1m02 <hkapray@gmaill.com>
2024-06-01 13:12:24 -0700

    Initial commit

---
+++ Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Raymond Kim on 3/7/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email address
+    let body: String // message content
+}
